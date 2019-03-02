db56fb8049fd59f9fd6dcfbc545440f4f7c54a42
xpom10 <xp10m@yandex.ru>
2019-03-02 19:03:52 +0300

    edit RegisterServiceImpl.java

--- Homework_1_Test/src/main/java/ru/otus/services/RegisterServiceImpl.java
+++ Homework_1_Test/src/main/java/ru/otus/services/RegisterServiceImpl.java
@@ -4,12 +4,9 @@ import ru.otus.model.User;
 
 public class RegisterServiceImpl implements RegisterService {
 
-    private User user;
-
     @Override
     public User registerUser(String name, String family) {
-        this.user = new User(name, family);
-        return user;
+        return new User(name, family);
     }
 
 }
