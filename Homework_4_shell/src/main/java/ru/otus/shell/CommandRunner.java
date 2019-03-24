a68c5d9795739a1f1dd1a73a922b3036786a1008
xpom10 <xp10m@yandex.ru>
2019-03-24 16:46:58 +0300

    add ShellMethodAvailability to CommandRunner.java

--- Homework_4_shell/src/main/java/ru/otus/shell/CommandRunner.java
+++ Homework_4_shell/src/main/java/ru/otus/shell/CommandRunner.java
@@ -1,14 +1,17 @@
 package ru.otus.shell;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.shell.Availability;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellMethodAvailability;
 import ru.otus.runner.TestRunnerService;
 
 @ShellComponent
 public class CommandRunner {
 
     private final TestRunnerService testRunnerService;
+    private boolean testIsPassed = false;
 
     @Autowired
     public CommandRunner(TestRunnerService testRunnerService) {
@@ -18,10 +21,19 @@ public class CommandRunner {
     @ShellMethod("Run a test")
     public void test() {
         testRunnerService.test();
+        testIsPassed = true;
     }
 
     @ShellMethod("Stop a test")
+    @ShellMethodAvailability("testIsPassed")
     public void stop() {
         System.exit(0);
+    }
+
+    public Availability testIsPassed() {
+        if (!testIsPassed) {
+            return Availability.unavailable("test not passed");
+        }
+        return Availability.available();
     }
 }
