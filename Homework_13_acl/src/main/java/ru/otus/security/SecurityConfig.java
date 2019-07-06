d74957239609193d5cc6dbfd5aa3327377524c94
Vivtanikmv <vivtanikmv@nspk.ru>
2019-07-06 17:10:20 +0300

    init Homework 13

---
+++ Homework_13_acl/src/main/java/ru/otus/security/SecurityConfig.java
@@ -0,0 +1,54 @@
+package ru.otus.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import ru.otus.security.service.MongoCustomDetailsService;
+
+@EnableWebSecurity
+@RequiredArgsConstructor(onConstructor = @__({@Autowired}))
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final MongoCustomDetailsService mongoCustomDetailsService;
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/resources/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                .and()
+                .authorizeRequests().antMatchers(HttpMethod.GET, "/api/**").permitAll()
+                .and()
+                .authorizeRequests().antMatchers("/**").authenticated()
+                .and()
+                .formLogin().defaultSuccessUrl("/books", true)
+                .and()
+                .rememberMe().key("someSecret");
+    }
+
+    @Autowired
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(mongoCustomDetailsService)
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    @SuppressWarnings("deprecation")
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
---
+++ Homework_13_acl/src/main/java/ru/otus/services/LibraryServiceImpl.java
@@ -0,0 +1,100 @@
+package ru.otus.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import ru.otus.domain.Author;
+import ru.otus.domain.Book;
+import ru.otus.domain.Comment;
+import ru.otus.domain.Genre;
+import ru.otus.dto.AuthorDto;
+import ru.otus.dto.BookDto;
+import ru.otus.dto.CommentDto;
+import ru.otus.dto.GenreDto;
+import ru.otus.repositories.AuthorRepository;
+import ru.otus.repositories.BookRepository;
+
+@Service
+@RequiredArgsConstructor(onConstructor = @__({@Autowired}))
+public class LibraryServiceImpl implements LibraryService {
+
+    private final BookRepository bookRepository;
+    private final AuthorRepository authorRepository;
+
+    @Override
+    public Flux<BookDto> findBooks() {
+        return bookRepository.findAll().map(Book::toDto);
+    }
+
+    @Override
+    public Mono<BookDto> findBook(String id) {
+        return bookRepository.findById(id).map(Book::toDto);
+    }
+
+    @Override
+    public Mono<BookDto> saveBook(BookDto bookDto) {
+        return authorRepository
+                .findAuthorByAuthorName(bookDto.getAuthor().authorName)
+                .switchIfEmpty(Mono.just(new Author(bookDto.getAuthor().authorName)))
+                .flatMap(authorRepository::save)
+                .map(authorMono -> {
+                    bookDto.getAuthor().setId(authorMono.getId());
+                    return bookDto;
+                })
+                .map(Book::fromDto)
+                .flatMap(bookRepository::save)
+                .map(Book::toDto);
+    }
+
+    @Override
+    public Mono<BookDto> editBook(String id, BookDto bookDto) {
+        return authorRepository
+                .findAuthorByAuthorName(bookDto.getAuthor().authorName)
+                .switchIfEmpty(Mono.just(new Author(bookDto.getAuthor().authorName)))
+                .flatMap(authorRepository::save)
+                .map(authorMono -> {
+                    bookDto.getAuthor().setId(authorMono.getId());
+                    return bookDto;
+                })
+                .map(Book::fromDto)
+                .map(book -> {
+                    book.setId(id);
+                    return book;
+                })
+                .flatMap(bookRepository::save)
+                .map(Book::toDto);
+    }
+
+    @Override
+    public Mono<Void> deleteBook(String id) {
+        return bookRepository.deleteById(id).then();
+    }
+
+    @Override
+    public Flux<GenreDto> findGenres() {
+        return bookRepository.findGenres().map(Genre::toDto);
+    }
+
+    @Override
+    public Mono<CommentDto> addComment(String id, CommentDto comment) {
+        Comment addcomment = Comment.fromDto(comment);
+        return bookRepository.addComment(id, addcomment).map(Comment::toDto);
+    }
+
+    @Override
+    public Flux<BookDto> findAuthorBooks(String id) {
+        return bookRepository.findBookByAuthor_Id(id).map(Book::toDto);
+    }
+
+    @Override
+    public Flux<AuthorDto> findAuthors() {
+        return authorRepository.findAll().map(Author::toDto);
+    }
+
+    @Override
+    public Mono<AuthorDto> findAuthor(String id) {
+        return authorRepository.findById(id).map(Author::toDto);
+    }
+}
